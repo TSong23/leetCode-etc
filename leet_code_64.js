a2d7d67dbfff8cfa492707d82a7dd4195a279d22
Taehoon <songyi3279@gmail.com>
2019-11-14 13:45:05 -0800

    working on 64

--- leet_code_64.js
+++ leet_code_64.js
@@ -1,6 +1,16 @@
 // Work through this problem on https://leetcode.com/problems/minimum-path-sum/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+// Input:
+// [
+//   [1, 3, 1],
+//   [1, 5, 1],
+//   [4, 2, 1]
+// ]
+// Output: 7
+// Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+
+
 function minPathSum(grid) {
 
 }
