6ed9e43cf2273aa7c64e5d2005b0f4a878d2717d
Taehoon <songyi3279@gmail.com>
2019-11-19 09:44:17 -0800

    70 solved

--- leet_code_70.js
+++ leet_code_70.js
@@ -2,7 +2,18 @@
 // Feel free to use this file for scratch work.
 
 function climbStairs(n) {
+  
+  let table = new Array(n);
+
+  table[0] = 1;
+  table[1] = 2;
+  
+  for (let i = 2; i < n; i++){
+    table[i] = table[i-1] + table[i-2]
+  }
+
+  return table[n-1];
 
 }
 
-//square fill
+// its just fibonacci
