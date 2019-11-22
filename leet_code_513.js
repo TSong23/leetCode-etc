c73f73bd55be5c03ed50707b8fda04f50e14d075
Taehoon <songyi3279@gmail.com>
2019-11-22 15:45:16 -0800

    leetcode 513 solved

---
+++ leet_code_513.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function (root) {
+  let queue = [[root, 0]];
+  let bottomLeft = [root.val, 0];
+
+  while (queue.length) {
+    let curDepth = queue[0][1];
+    let curNode = queue[0][0];
+    if (curNode.left) {
+      queue.push([curNode.left, curDepth + 1]);
+    }
+    if (curNode.right) {
+      queue.push([curNode.right, curDepth + 1]);
+    }
+    if (!curNode.left && !curNode.right) {
+      if (curDepth > bottomLeft[1]) {
+        bottomLeft = [curNode.val, curDepth];
+      }
+    }
+    queue.shift();
+
+  }
+
+  return bottomLeft[0];
+
+};
