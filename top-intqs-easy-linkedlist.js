aa16d268bd97e04d3c7fa830a270c68f133ba3a0
Taehoon <songyi3279@gmail.com>
2019-11-28 14:49:05 -0800

    remove nth node from linked list doesnt work for linked lists shorter than 4

--- top-intqs-easy-linkedlist.js
+++ top-intqs-easy-linkedlist.js
@@ -6,3 +6,32 @@ var deleteNode = function (node) {
 
 
 // remove Nth node from End of List
+var removeNthFromEnd = function (head, n) {
+
+  // let i count the length of the linked list
+  // i - n gives the remove node
+  // n = 1 => remove last node
+  // n = 3 => linked list must be at least 3
+
+  if (head.next === null){
+    return null;
+  }
+
+  let i = 0;
+  let currentNode = head;
+  
+  for (i ; i < n-1; i++){
+    currentNode = currentNode.next;
+  }
+    
+  let removeNode = head;
+  while (currentNode.next !== null){
+    currentNode = currentNode.next;
+    removeNode = removeNode.next;
+  }
+
+  removeNode.val = removeNode.next.val;
+  removeNode.next = removeNode.next.next;
+
+  return head;
+};
