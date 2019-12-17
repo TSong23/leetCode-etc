46190d790d3158cd6050a3c9d42146c14e94c585
Taehoon <songyi3279@gmail.com>
2019-12-17 12:28:15 -0800

    close to solving zigzag inorder tree

--- top-intqs-med/trees-graphs.js
+++ top-intqs-med/trees-graphs.js
@@ -29,3 +29,60 @@ var inorderTraversal = function (root) {
 
 
 };
+
+
+// binary tree zigzag traversal
+// Given a binary tree, return the zigzag level order traversal of its nodes' values.
+// (ie, from left to right, then right to left for the next level and alternate between).
+
+//    3
+//   / \
+  // 9  20
+  //   /  \
+  // 15   7
+// return its zigzag level order traversal as:
+// [
+//   [3],
+//   [20, 9],
+//   [15, 7]
+// ]
+
+
+// breath first search
+// but have to switch left to right and right to left
+// keep a switch or L and R
+// also, need to keep track of the level
+// level tracker % 2. 
+
+var zigzagLevelOrder = function (root) {
+  if (!root) return [];
+  let reArr = [];
+
+  let reQueue = [ [root, 0]];
+  let leftright = 0;
+
+  while (reQueue.length) {
+    let shiftArr = reQueue.shift();
+    let node = shiftArr[0];
+    let level = shiftArr[1];
+
+    if (!reArr[level]){
+      reArr.push([node.val]);
+    }else {
+      reArr[level].push(node.val);
+    }    
+
+    if (leftright === 0) {
+      if (node.right) reQueue.push( [node.right, level + 1);
+      if (node.left) reQueue.push([node.left, level + 1 ]);
+      leftright = 1;
+    } else {
+      if (node.left) reQueue.push([node.left, level + 1]);
+      if (node.right) reQueue.push([node.right, level + 1]);
+      leftright = 0;
+    }
+  }
+
+  return reArr;
+  
+};
