474d2e5fe172e82feca60dfea80ad5c842ef9d5a
Taehoon <songyi3279@gmail.com>
2020-01-20 14:20:25 -0800

    half way done. wrote moving to front code and evicting node. will need to refactor for helper functions

--- LRU_cache.js
+++ LRU_cache.js
@@ -5,47 +5,87 @@ var LRUCache = function (capacity) {
 
   this.length = 0;
   this.myHash = {};
-  this.head = new Node(null);
-  this.tail = new Node(null);
+  this.head = new Node('head', null);
+  this.tail = new Node('tail', null);
+
+
 
 
 };
 
-// /** 
-//  * @param {number} key
-//  * @return {number}
-//  */
+
 LRUCache.prototype.get = function (key) {
   if (this.myHash[key]){
     // move to front of linked list
     // helper function moveToFront()
+  } else {
+    return -1;
   }
 
 };
 
-// /** 
-//  * @param {number} key 
-//  * @param {number} value
-//  * @return {void}
-//  */
+
 LRUCache.prototype.put = function (key, value) {
   // if key exists, move to front with new value
   // else, insert new key value pair at front
   // also, check if length of the LRU cache is at limit
 
-  if (this.length > capacity){
-    // function moveToFront()
+  if(this.myHash[key]){ 
+
+    // move to front function
+
+    // movingNode = node moving to front
+    // prevFirst = previous first in line
+    let prevFirst = this.head.next;
+    let movingNode = this.myHash[key];
+
+    // cut out the moving node first
+    let movePrev = movingNode.prev;
+    let moveNext = movingNode.next;
+    movePrev.next = moveNext;
+    moveNext.prev = movePrev;
+
+    // insert moving at front
+    this.head.next = movingNode;
+    movingNode.prev = this.head;
+    movingNode.next = prevFirst;
+    prevFirst.prev = movingNode;
+
+    // now the nodes have been replaced
+
   } else {
-    // function insertAtFront()
-    // this.length++;
-    
+    if (this.length >= capacity) {
+      // new key and capacity reached
+      // evict last node
+      // place new node at front
+
+      // evict last node and delete the key from hash
+      let evictNode = this.tail.prev;
+      this.tail.prev = evictNode.prev;
+      evictNode.prev.next = this.tail;
+      delete this.myHash[evictNode.key];
+
+      // create new node and place new node at front
+      this.myHash[key] = new Node(key, value);
+      let movingNode = this.myHash[key];
+      let prevFirst = this.head.next;
+      this.head.next = movingNode;
+      movingNode.prev = this.head;
+      movingNode.next = prevFirst;
+      prevFirst.prev = movingNode;
+
+    } else {
+      // function insertAtFront()
+      // this.length++;
+    } 
   }
 
 };
 
 
 class Node {
-  constructor(value){
+  constructor(key, value){
+    this.key;
     this.value = value;
     this.prev = null;
     this.next = null;
