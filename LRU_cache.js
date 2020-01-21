35a76884e0ad7f8b69ceab46674e5639a41c9df2
Taehoon <songyi3279@gmail.com>
2020-01-21 10:56:12 -0800

    IXL onsite prep leetcode 592

--- LRU_cache.js
+++ LRU_cache.js
@@ -2,22 +2,51 @@
 //  @param {number} capacity
 // */
 var LRUCache = function (capacity) {
-
+  this.capacity = capacity;
   this.length = 0;
   this.myHash = {};
   this.head = new Node('head', null);
   this.tail = new Node('tail', null);
 
-
-
+  //initialize by connecting head and tail
+  this.head.next = this.tail;
+  this.tail.prev = this.head;
 
 };
 
 
+
 LRUCache.prototype.get = function (key) {
   if (this.myHash[key]){
-    // move to front of linked list
-    // helper function moveToFront()
+
+    // if its already at front then don't move it
+    if (this.myHash[key] === this.head.next) {
+      return this.myHash[key].value;
+    }
+
+    // move to front function
+
+    // movingNode = node moving to front
+    // prevFirst = previous first in line
+    let prevFirst = this.head.next;
+    let movingNode = this.myHash[key];
+
+    // cut out the moving node first
+    let movePrev = movingNode.prev;
+    let moveNext = movingNode.next;
+    movePrev.next = moveNext;
+    moveNext.prev = movePrev;
+
+    // insert moving at front
+    this.head.next = movingNode;
+    movingNode.prev = this.head;
+    movingNode.next = prevFirst;
+    prevFirst.prev = movingNode;
+
+    // return the value of the key
+    return movingNode.value;
+
+    
   } else {
     return -1;
   }
@@ -54,7 +83,7 @@ LRUCache.prototype.put = function (key, value) {
     // now the nodes have been replaced
 
   } else {
-    if (this.length >= capacity) {
+    if (this.length >= this.capacity) {
       // new key and capacity reached
       // evict last node
       // place new node at front
@@ -76,7 +105,20 @@ LRUCache.prototype.put = function (key, value) {
 
     } else {
       // function insertAtFront()
-      // this.length++;
+      this.myHash[key] = new Node(key, value);
+      let movingNode = this.myHash[key];
+
+
+      let prevFirst = this.head.next;
+
+      this.head.next = movingNode;
+      movingNode.prev = this.head;
+
+      movingNode.next = prevFirst;
+      prevFirst.prev = movingNode;
+
+      this.length++;
+      
     } 
   }
 
@@ -85,7 +127,7 @@ LRUCache.prototype.put = function (key, value) {
 
 class Node {
   constructor(key, value){
-    this.key;
+    this.key = key;
     this.value = value;
     this.prev = null;
     this.next = null;
@@ -99,14 +141,14 @@ class Node {
  * obj.put(key,value)
  */
 
-// LRUCache cache = new LRUCache(2 /* capacity */);
+let cache = new LRUCache(2);
+cache.put(2,1);
+cache.put(2, 2);
+cache.get(2);
+cache.put(1, 1);
+cache.put(4, 1);
+cache.get(2);
 
-// cache.put(1, 1);
-// cache.put(2, 2);
-// cache.get(1);       // returns 1
-// cache.put(3, 3);    // evicts key 2
-// cache.get(2);       // returns -1 (not found)
-// cache.put(4, 4);    // evicts key 1
-// cache.get(1);       // returns -1 (not found)
-// cache.get(3);       // returns 3
-// cache.get(4);       // returns 4
+
+
+
---
+++ leet_code_592.js
@@ -0,0 +1,13 @@
+// Input: "-1/2+1/2"
+// Output: "0/1"
+
+// Input: "-1/2+1/2+1/3"
+// Output: "1/3"
+
+// Input: "5/3+1/3"
+// Output: "2/1"
+
+var fractionAddition = function (expression) {
+
+  
+};
