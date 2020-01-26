b5a6c9da889d45cacfb74a245d0e4331e68b7183
Taehoon <songyi3279@gmail.com>
2020-01-25 16:54:02 -0800

    begin number of islands problem

--- top-intqs-med/trees-graphs.js
+++ top-intqs-med/trees-graphs.js
@@ -193,3 +193,20 @@ var connect = function (root) {
 };
   
 
+/////////Number of Islands
+// Input:
+// 11110
+// 11010
+// 11000
+// 00000
+
+// Output: 1
+
+// Input:
+// 11000
+// 11000
+// 00100
+// 00011
+
+// Output : 3
+
