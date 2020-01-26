e2fb614f228bd0be4bfc0189f9cbbe810aabcd8d
Taehoon <songyi3279@gmail.com>
2020-01-25 16:55:43 -0800

    already have whiteboarded. can solve O(n) time and O(n) space

--- top-intqs-med/trees-graphs.js
+++ top-intqs-med/trees-graphs.js
@@ -194,6 +194,11 @@ var connect = function (root) {
   
 
 /////////Number of Islands
+
+// Given a 2d grid map of '1's(land) and '0's(water), count the number of 
+// islands.An island is surrounded by water and is formed by connecting 
+// adjacent lands horizontally or vertically.You may assume all four edges of
+//  the grid are all surrounded by water.
 // Input:
 // 11110
 // 11010
@@ -210,3 +215,7 @@ var connect = function (root) {
 
 // Output : 3
 
+
+var numIslands = function (grid) {
+
+};
