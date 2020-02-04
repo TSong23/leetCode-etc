2a7412b48139c5b5802a62cff5f156995843184b
Taehoon <songyi3279@gmail.com>
2020-02-04 10:16:55 -0800

    start lowest common descendent of binary tree

---
+++ binary_tree/236.js
@@ -0,0 +1,15 @@
+//////////////////Lowest Common Ancestor of a Binary Tree///////////////////////
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+ // node is allowed to be a descendent of itself
+
+var lowestCommonAncestor = function (root, p, q) {
+
+};
