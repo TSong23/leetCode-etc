cd1d8ddb6408733ae5bc3d9b136432de38c826af
Taehoon <songyi3279@gmail.com>
2020-02-08 21:57:40 -0800

    spiral array problem

---
+++ array/59.js
@@ -0,0 +1,17 @@
+var generateMatrix = function (n) {
+
+  let max = n * n;
+  let reArr = [[]];
+
+  for (let i = 1; i < n + 1; i++){
+    reArr[0].push(i);
+  }
+  reArr.push([]);
+  reArr[1][2] = 4;
+
+
+
+};
+
+
+generateMatrix(3);
