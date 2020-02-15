582ad4d9a029ed973df314fa3b09cd6db1b6f61d
Taehoon <songyi3279@gmail.com>
2020-02-15 15:15:39 -0800

    begin solving 807

---
+++ array/807.js
@@ -0,0 +1,20 @@
+
+
+
+var maxIncreaseKeepingSkyline = function (grid){
+
+  let OriCount = 0; 
+  let rowMax = [Math.max(...grid[0])];
+
+
+
+};
+
+
+let myTestGrid = [
+  [3, 0, 8, 4],
+  [2, 4, 5, 7],
+  [9, 2, 6, 3],
+  [0, 3, 1, 0]
+]
+maxIncreaseKeepingSkyline(myTestGrid);
