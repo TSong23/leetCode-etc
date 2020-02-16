ed6f3cca248d4f4c74532b692521e5b1e655a958
Taehoon <songyi3279@gmail.com>
2020-02-15 16:02:16 -0800

    solved 807. beats 96.36%

--- array/807.js
+++ array/807.js
@@ -4,9 +4,29 @@
 var maxIncreaseKeepingSkyline = function (grid){
 
   let OriCount = 0; 
-  let rowMax = [Math.max(...grid[0])];
+  let NewCount = 0;
+  let rowMax = [];
+  let colMax = [];
 
+  for (let i = 0; i < grid.length; i++){
+    rowMax.push(Math.max(...grid[i]));
+    let colSub = [];
+    for (let j = 0; j < grid.length; j++){
+      colSub.push(grid[j][i]);
+    }
+    OriCount = OriCount + colSub.reduce((a,b) => a + b, 0);
+    colMax.push(Math.max(...colSub));
+  }
 
+  for (let i = 0; i < grid.length; i++){
+    let rowSub = [];
+    for (let j = 0; j < grid[i].length; j++){
+      colMax[j] > rowMax[i] ? rowSub.push(rowMax[i]) : rowSub.push(colMax[j]);
+    }
+    NewCount = NewCount + rowSub.reduce((a,b) => a + b, 0);
+  }
+
+  return NewCount - OriCount;
 
 };
 
