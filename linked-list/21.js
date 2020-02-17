0eca0d97e7652d6205236e8eee34d696f0a8abc5
Taehoon <songyi3279@gmail.com>
2020-02-16 18:16:48 -0800

    solved merge two lists

--- linked-list/21.js
+++ linked-list/21.js
@@ -1,3 +1,22 @@
 var mergeTwoLists = function (l1, l2) {
+  let head = new ListNode();
+  let tail = head;
 
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      tail.next = l1;
+      l1 = l1.next;
+    } else {
+      tail.next = l2;
+      l2 = l2.next;
+    }
+    tail = tail.next;
+  }
+
+  if (l1) tail.next = l1;
+  if (l2) tail.next = l2;
+
+  return head.next;
 };
+
