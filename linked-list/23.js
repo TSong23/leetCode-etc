11fa4e847aaaf27ef69af4cdf04824541ef334bf
Taehoon <songyi3279@gmail.com>
2020-02-21 18:09:31 -0800

    found brute force solution to leetcode 23

--- linked-list/23.js
+++ linked-list/23.js
@@ -6,6 +6,28 @@
 // ]
 // Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6
 
+// lists is array of list heads
+// try brute force: a forloop inside while loop
+
+// use the while loop: if the smallestHead = Infinity, then it means all the lists ran out
+
+
 var mergeKLists = function (lists) {
+
+  let smallestHead = Infinity;
+  let myListHead = ListNode(null);
+  let myListTail;
+
+  lists.forEach(head => {
+    if(head && head.val < smallestHead){
+      smallestHead = head;
+    }
+  })
+
+  myListTail = smallestHead;
+  myListHead.next = myListTail;
+
+
+  console.log(smallestHead);
 
 };
