fd22a7517075f86ae90f8da6f51fcd5ba1d57776
Taehoon <songyi3279@gmail.com>
2020-03-23 03:37:17 -0700

    try BFT

--- binary_tree/199.js
+++ binary_tree/199.js
@@ -1,14 +1,25 @@
 
 
-// while the node in question has children, check if it has right
-// push to sideview array
-// else push left to side view
+// use BFS and add the last node of each level
 
-// now if no children, we must check other side of the root
-// go down to the correct length, then begin adding right or left side
-
-// 2 helpers: add to sideview arr and traverse leftside of root
 
 var rightSideView = function (root) {
+  if (!root) return [];
 
+  let myQueue = [[root]];
+  let sideView = [];
+  let newArr = [];
+  while(myQueue.length){
+    let popArr = myQueue.shift();
+    sideView.push(popArr[popArr.length-1]);
+    popArr.forEach(el => {
+      if(el){
+        newArr.push(el.left);
+        newArr.push(el.right);
+      };
+    });
+    myQueue.push(newArr);
+    newArr = [];
+  };
+  return sideView;
 };
