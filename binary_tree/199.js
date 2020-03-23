55c7d2c7bad24e330db770e2054427834e305171
Taehoon <songyi3279@gmail.com>
2020-03-23 01:08:08 -0700

    leetcode 199 begin

---
+++ binary_tree/199.js
@@ -0,0 +1,14 @@
+
+
+// while the node in question has children, check if it has right
+// push to sideview array
+// else push left to side view
+
+// now if no children, we must check other side of the root
+// go down to the correct length, then begin adding right or left side
+
+// 2 helpers: add to sideview arr and traverse leftside of root
+
+var rightSideView = function (root) {
+
+};
