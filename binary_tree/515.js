24d5c9621daabffeca814962d772722aa66970b0
Taehoon <songyi3279@gmail.com>
2020-03-24 10:55:18 -0700

    solved 515

--- binary_tree/515.js
+++ binary_tree/515.js
@@ -8,5 +8,23 @@
 
 // use BFS and push values in to array
 var largestValues = function (root) {
-
+  if (!root) return [];
+  let reArr = [];
+  let myQueue = [[root]];
+  let thisLvl;
+  let largest;
+  let nextLvl = [];
+  while (myQueue.length) {
+    thisLvl = myQueue.shift();
+    nextLvl = [];
+    largest = thisLvl[0].val;
+    thisLvl.forEach(node => {
+      if (node.val > largest) largest = node.val;
+      if (node.left) nextLvl.push(node.left);
+      if (node.right) nextLvl.push(node.right);
+    });
+    reArr.push(largest);
+    if (nextLvl.length) myQueue.push(nextLvl);
+  };
+  return reArr;
 };
