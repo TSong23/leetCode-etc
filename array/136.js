25e7476a038bd55fe4ce9d91fb766002cdb16c07
Taehoon <songyi3279@gmail.com>
2020-04-02 06:06:44 -0700

    April Challenge Day 1: 136 solved

---
+++ array/136.js
@@ -0,0 +1,18 @@
+
+var singleNumber = function (nums) {
+  let myHash = {};
+  nums.forEach(num => {
+    if (myHash[num]) {
+      myHash[num]++;
+    } else {
+      myHash[num] = 1;
+    }
+  });
+
+  let myNum;
+  Object.keys(myHash).forEach(key => {
+    if (myHash[key] === 1) myNum = key;
+  });
+
+  return myNum;
+};
