cafe8d774a5f888f8d6e133a4eda6ca96c162944
Taehoon <songyi3279@gmail.com>
2020-04-06 11:01:51 -0700

    lc30 April D6 solved. I once again took something simple and turned it complicated

--- hash/49.js
+++ hash/49.js
@@ -10,6 +10,7 @@
 // 2. make hash of each str
 // 3. ate => {'a':1, 't':1, 'e':1}
 // 4. make second hash: {'a1e1t1': ['ate', 'eat']}
+//    -hash does not auto alphabetize keys
 // 5. return reArr.push(Object.values(secondHash));
 
 
@@ -21,8 +22,26 @@ var groupAnagrams = function (strs) {
 
   strs.forEach(ele => {
     let letterHash = makeLHash(ele);
-    
+    let hashKey = makeKey(letterHash);
+    if (wordHash[hashKey]){
+      wordHash[hashKey].push(ele);
+    } else{
+      wordHash[hashKey] = [ele];
+    };
   });
+
+  Object.values(wordHash).forEach(arr => reArr.push(arr));
+  return reArr;
+};
+
+function makeKey(myHash){
+  let reStr = [];
+  let charArr = Object.keys(myHash).sort();
+  charArr.forEach(char => {
+    reStr.push(char);
+    reStr.push(myHash[char]);
+  });
+  return reStr.join('');
 };
 
 function makeLHash(str){
@@ -32,9 +51,10 @@ function makeLHash(str){
     if (reHash[char]){
       reHash[char]++;
     } else{
-      reHash[char] = 0;
+      reHash[char] = 1;
     };
   });
+  return reHash;
 };
 
 groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]);
