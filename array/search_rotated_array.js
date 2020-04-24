ea7209000d0a8bdbf79c73b7bf536583432a9434
Taehoon <songyi3279@gmail.com>
2020-04-24 07:40:32 -0700

    fell behind on the challenges

--- array/search_rotated_array.js
+++ array/search_rotated_array.js
@@ -1,9 +1,10 @@
 // Input: nums = [4, 5, 6, 7, 0, 1, 2], target = 0
 // Output: 4
 
-
-
+// search from end if the end is less than the start
+// calculate the rotated amount
+// find the target
 var search = function (nums, target) {
-
+  
 };
 
