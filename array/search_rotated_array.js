ffeed33658b019c4c30f6cd4cb6a5fe8212d01cd
Taehoon <songyi3279@gmail.com>
2020-04-29 07:22:00 -0700

    too many if statements

--- array/search_rotated_array.js
+++ array/search_rotated_array.js
@@ -6,15 +6,33 @@
 // find the target
 var search = function (nums, target) {
 
+  if (!nums.length) return -1;
+  if (nums.length < 2) {
+    if (nums[0] === target) {
+      return 0;
+    } else {
+      return -1;
+    };
+  };
   let numRo;
   let numLen = nums.length;
-  for(let i =1; i < nums.length; i++){
-    if(nums[numLen - i] > nums[0]){
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[numLen - i] > nums[0]) {
       numRo = i;
       break;
     };
   };
-  return numRo;
+  let spliceArr = nums.splice(numRo);
+  let newArr = spliceArr.concat(nums);
+  let myIdx = newArr.indexOf(target);
+  if (myIdx < 0) return -1;
+  console.log('numRo:', numRo);
+  console.log('numLen:', numLen);
+  if (numRo >= numLen - 1) {
+    return myIdx;
+  } else {
+    return myIdx + numRo;
+  }
 };
 
 console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
