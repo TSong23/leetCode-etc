fa68d5b67d41e8c801374b8cd0b72e95f821922b
Taehoon <songyi3279@gmail.com>
2020-05-12 07:14:57 -0700

    leetcode challenge May 12 start

---
+++ recursion/floodfill.js
@@ -0,0 +1,23 @@
+/*
+  image = [
+           [1,1,1]
+           [1,1,0]
+           [2,0,1]
+          ]
+
+  if we start with the 1 in the center, then change all 1's connected
+  to it to 2, we end up with
+  
+  image = [
+           [2,2,2]
+           [2,2,0]
+           [2,0,1]
+          ]
+
+  perform bst
+*/
+
+
+var floodFill = function (image, sr, sc, newColor) {
+
+};
