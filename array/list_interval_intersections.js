86cb14a6e862759be0f64a7c8b6a50b2f79ac8ef
Taehoon <songyi3279@gmail.com>
2020-05-23 18:27:21 -0700

    begin list interval intersections. leetcode challenge may 23

---
+++ array/list_interval_intersections.js
@@ -0,0 +1,11 @@
+/*
+Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+*/
+
+
+
+var intervalIntersection = function (A, B) {
+
+};
