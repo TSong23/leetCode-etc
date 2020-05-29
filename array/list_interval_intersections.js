dd754c65115b5959d010fe29f6f446b247630b86
Taehoon <songyi3279@gmail.com>
2020-05-29 06:52:07 -0700

    leetcode may challenge updated

--- array/list_interval_intersections.js
+++ array/list_interval_intersections.js
@@ -9,23 +9,34 @@ Reminder: The inputs and the desired output are lists of Interval objects, and n
 
   AStart, AEnd
   BStart, BEnd
-  
+
   1. start count when the number is within A and B
   2. when condition 1 is false, add last number to count Arr. countArr to output
   3. shift A and B when the count is greater than AEnd or BEnd.
+
+  need to finish this
 */
 
 var intervalIntersection = function (A, B) {
 
   let myOutput = [];
-  let myCount; 
-  let AStart, AEnd, BStart, BEnd;
-
+  let count = 0;
+  let countRange = [0];
+  
   let curA = A.shift();
   let curB = B.shift();
+  
+  let AStart = curA[0];
+  let AEnd = curA[1];
+  let BStart = curB[0];
+  let BEnd = curB[1];
 
   while(A.length || B.length){
-  
+    if(count >= AStart && count <= AEnd){
+      if (count >= BStart && count <= BEnd){
+        
+      }
+    }
 
   }
 
