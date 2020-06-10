9ac558c315ad7e4cf2686e5a579414297630523a
Taehoon <songyi3279@gmail.com>
2020-06-09 18:15:48 -0700

    leetcode june challenge. day 9 done

---
+++ stack/392.js
@@ -0,0 +1,17 @@
+var isSubsequence = function (s, t) {
+  let sArr = s.split('');
+  let tArr = t.split('');
+
+  if(sArr.length === 0) return true;
+  for(let i=0; i< tArr.length; i++){
+    if(tArr[i] === sArr[0]){
+      sArr.shift();
+      if(sArr.length === 0){
+        return true;
+      };
+    };
+  };
+
+  return false;
+
+};
