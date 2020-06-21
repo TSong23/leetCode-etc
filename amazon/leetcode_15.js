cff9208ad408da12a51a29b73810780113f8a9e1
Taehoon <songyi3279@gmail.com>
2020-06-21 01:04:56 -0700

    found the brute force method

--- amazon/leetcode_15.js
+++ amazon/leetcode_15.js
@@ -15,8 +15,35 @@
 //   [-1, -1, 2]
 // ]
 
-function sum(arr){
+// the brute force way is to generate all unique combinations of three 
+// then go through each and check if it is zero
 
+function threeSum(nums){
+
+  let arrLen  =nums.length;
+  let arrCom = [];
+  let reArr = [];
+  let myHash = {};
+  for(let i = 0; i<arrLen-2;i++){
+    for(let j = i+1; j < arrLen - 1; j++){
+      for(let k = j+1; k < arrLen; k++){
+        let cur = [nums[i], nums[j], nums[k]];
+        let cur2 = cur.sort();
+        if(myHash[cur2]){
+          continue;
+        }else{
+          myHash[cur2] = 1;
+          arrCom.push(cur);
+        }                
+      };
+    };
+  };
+  arrCom.forEach(arr => {
+    if(arr.reduce(function(a,b){return a+b}) === 0) reArr.push(arr);
+  });
+  return reArr;
 };
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
 
 
