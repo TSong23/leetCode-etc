f4c011422b7a47b88aa6a09ea70b536af80e024b
Taehoon <songyi3279@gmail.com>
2020-06-27 23:14:06 -0700

    leetocde 435 start

---
+++ amazon/leetcode_435.js
@@ -0,0 +1,22 @@
+// Input: [[1, 2], [2, 3], [3, 4], [1, 3]]
+// Output: 1
+// Explanation: [1, 3] can be removed and the rest of intervals are non - overlapping.
+
+// Input: [[1, 2], [2, 3]]
+// Output: 0
+// Explanation: You don't need to remove any of the intervals since they're already non - overlapping.
+
+
+// [[1, 2], [2, 3], [3, 4], [1, 3]]. removing [1,3] rather than [1,2], [2,3]
+// [1, 3] covers more distance, so preferable to remove this one. this is condition for removal
+
+
+
+
+
+// Brute force way: 
+
+var eraseOverlapIntervals = function (intervals) {
+
+
+};
