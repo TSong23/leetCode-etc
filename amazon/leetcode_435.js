681b22a62e7403bbe5ec3736a3544a8a5dc9a1e3
Taehoon <songyi3279@gmail.com>
2020-06-28 13:54:35 -0700

    found a new approach on problem 435

--- amazon/leetcode_435.js
+++ amazon/leetcode_435.js
@@ -11,38 +11,40 @@
 // [1, 3] covers more distance, so preferable to remove this one. this is condition for removal
 // how to keep track of overlaps
 
-// sorted array: sort by interval start(smaller) and then by interval end(larger)
-// take the first interval, the end value and put a pointer to that value
+// sorted array: sort by interval end(smaller) and then by interval start(larger)
+
+// [1 2 3 4 5]
+//   [2   4]
+//       [4  5]
 // then go to the next interval that starts with pointer, and save idx value of those interval
 // when reach the end, return intervals.length - idx.length
 
 
 var eraseOverlapIntervals = function (intervals) {
-
-  if(intervals.length <= 1) return 0;
+  if (intervals.length <= 1) return 0;
 
   let mySort = intervals.sort(softFunction);
-  function softFunction(a,b){
-    if(a[0] === b[0]){
-      if(a[1] === b[1]) return 0;
+  function softFunction(a, b) {
+    if (a[1] === b[1]) {
+      if (a[0] === b[0]) return 0;
+      return a[0] < b[0] ? 1 : -1;
+    } else {
       return a[1] < b[1] ? -1 : 1;
-    }else{
-      return a[0] < b[0] ? -1 : 1;
     };
   };
 
   let idxArr = [0];
   let myPtr = mySort[0][1];
   let arrLen = mySort.length;
-  
-  for(let i =1; i < arrLen; i++){
-    if(mySort[i][0] >= myPtr){
+
+  for (let i = 1; i < arrLen; i++) {
+    if (mySort[i][0] >= myPtr) {
       idxArr.push(i);
       myPtr = mySort[i][1];
     };
   };
 
-  return arrLen - idxArr.length;
+  return arrLen - idxArr.length; 
 };
 
 eraseOverlapIntervals(
