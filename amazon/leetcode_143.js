1bceecf13c97348ae4e1ec26fcdf0d19c2f8d59f
Taehoon <songyi3279@gmail.com>
2020-07-07 21:33:00 -0700

    begin leetcode 143

---
+++ amazon/leetcode_143.js
@@ -0,0 +1,14 @@
+// Given a singly linked list L: L0→L1→…→Ln - 1→Ln,
+//   reorder it to: L0→Ln→L1→Ln - 1→L2→Ln - 2→…
+
+// You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+//brute force: have two pointers, head and tail.
+//just begin inserting one by one
+
+
+
+var reorderList = function (head) {
+
+};
+
