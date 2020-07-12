013a9f86b50a4bea5b6436d89063baecfa4f1e00
Taehoon <songyi3279@gmail.com>
2020-07-12 13:20:39 -0700

    begin leetcode 139

---
+++ amazon/leetcode_139.js
@@ -0,0 +1,42 @@
+// Input: s = "leetcode", wordDict = ["leet", "code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+// Input: s = "applepenapple", wordDict = ["apple", "pen"]
+// Output: true
+// Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+// Note that you are allowed to reuse a dictionary word.
+
+// Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+// Output: false
+
+// initial thoughts: dp problem => break up in to smaller bits
+// small bit 1: confirm if a word from Dict exists by iteration or stack
+// small bit 2: once chars in String is used, cannot be reused. so remove the chars and break up the string in to separate sections
+// small bit 3: once a word is found, go on and find the next one from Dict
+
+// repeat small bits
+// also, we can count the amount of chars in dict and if > chars in S, return false
+
+var wordBreak = function (s, wordDict) {  
+  //check for chars in Dict > chars in s
+  let charCount = 0;
+  wordDict.forEach(element => {
+    charCount += element.length;
+  });
+  if (charCount > s.length) return false;
+
+  // select first word from wordDict
+  let startWord = wordDict[0];
+  let startChar = startWord[0];
+
+  for(let i =0; i< s.length; i++){
+    if (s[i] === startChar){
+      // start search
+    };
+  };
+
+
+};
+
+console.log(wordBreak("leetcode",["leet", "code"]))
--- array/448.js
+++ array/448.js
@@ -6,5 +6,18 @@
 // [5, 6]
 
 var findDisappearedNumbers = function (nums) {
-
+  nums.sort();
+  count = 0;
+  let reArr = [];
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] === count + 1){
+      count+=1;
+    }else{
+      reArr.push(count+1);
+      count+=1;
+    };
+  };
+  return reArr;
 };
+
+findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]);
