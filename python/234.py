adb1cd9f53d92d17b2b7f1ff137e8620351ca99c
Taehoon <songyi3279@gmail.com>
2020-07-19 21:28:16 -0700

    solved 234 in python

--- python/234.py
+++ python/234.py
@@ -3,12 +3,39 @@
 # // take the head on one list the halves, reverse it
 # // them compare the two lists
 
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# 1 2 3 2 1
+#   s f 
+#     s   f
+#     r s f
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def isPalindrome(self, head):
-        fast, slow = head
+        if(not head or not head.next): return True
+        fast = slow = head
+        while (fast and fast.next):
+            slow=slow.next
+            fast = fast.next.next
         
+        rev = ListNode(slow.val)
+        slow=slow.next
+
+        while(slow):
+            prev=rev
+            curr=ListNode(slow.val)
+            curr.next = prev
+            rev = curr
+            slow=slow.next            
+
+        while(rev):
+            if(rev.val!=head.val):
+                return False
+            rev=rev.next
+            head=head.next
+        return True
+
