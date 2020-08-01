364aa1cab5cc719e842c1666d2a8607244a3768f
Taehoon <songyi3279@gmail.com>
2020-08-01 16:50:33 -0700

    solved work break via tabulation methods

--- interview_q/leetcode_139.js
+++ interview_q/leetcode_139.js
@@ -11,23 +11,19 @@
 // Output: false
 
 // initial thoughts: dp problem => break up in to smaller bits
-// small bit 1: confirm if a word from Dict exists by iteration or stack
-// For each index i see if we can find a word from the available words in the substring s[i..j) (ie. i inclusive to j non-inclusive). If found, then we can reach j. Recursively continue this process with the goal of reaching the end index N of the input string s. Return true if and only if we can reach N.
-
+// solve by using tabulation or memoization
 
 var wordBreak = function (s, wordDict) {  
-  let idx = 0;
-  let hold = s;
-  while(hold.length){
-    idx++;
-    let wordSeg = hold.slice(0,idx);
-    if (wordDict.includes(wordSeg)) {
-      hold = hold.slice(idx);
-      idx = 0;
-    };    
-    if(idx > hold.length) return false;
-  }
-  return true;
+  let table = [];
+
+  for(let len = 1; len <=s.length; len++){
+    for(let idx = 0; idx < len; idx++){
+      if(table[idx] && wordDict.includes(s.slice(i,len))){
+        table[len] = true;
+      };
+    };
+  };
+  return table[s.length]||false;
 };
 
 
