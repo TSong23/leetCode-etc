3c61a032b18c8ccc39e56aba2cfb7a6d4ff51bf4
Taehoon <songyi3279@gmail.com>
2020-08-01 01:18:22 -0700

    finally debugged LRU cache. 146.js solved

--- LRU_cache.js
+++ LRU_cache.js
@@ -1,150 +1,121 @@
 // /**
 //  @param {number} capacity
 // */
+
+// use a doubly linked list and hash map
+// doubly linked list node key is key to hash map
+// hashmap stores the node; ensures 0(1) access of any node
+
+class Node {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  };
+};
+
 var LRUCache = function (capacity) {
   this.capacity = capacity;
   this.length = 0;
   this.myHash = {};
-  this.head = new Node('head', null);
+  this.head = new Node('head',null);
   this.tail = new Node('tail', null);
-
-  //initialize by connecting head and tail
   this.head.next = this.tail;
   this.tail.prev = this.head;
-
 };
 
 
+LRUCache.prototype.moveFront = function(key){
+  // if already at front, return
+  if (key === this.head.next.key) return;
+  // movingNode = node moving to front
+  // prevFirst = previous first in line
+  let prevFirst = this.head.next;
+  let movingNode = this.myHash[key];
+
+  // cut out the moving node first
+  let movePrev = movingNode.prev;
+  let moveNext = movingNode.next;
+  movePrev.next = moveNext;
+  moveNext.prev = movePrev;
+
+  // insert moving at front
+  this.head.next = movingNode;
+  movingNode.prev = this.head;
+  movingNode.next = prevFirst;
+  prevFirst.prev = movingNode;
+
+  return;
+};
+
+LRUCache.prototype.deleteLast = function(){
+  let prevLast = this.tail.prev;
+  let newLast = prevLast.prev;
+  // cut prevLast
+  newLast.next = this.tail;
+  this.tail.prev = newLast;
+  prevLast.next = null;
+  prevLast.prev = null;
+
+  this.myHash[prevLast.key] = null;
+
+  return;
+};
+
+LRUCache.prototype.insertFront = function (newNode){
+  let prevFirst = this.head.next;
+  // cut previous first
+  this.head.next = newNode;
+  prevFirst.prev = newNode;
+  newNode.prev = this.head;
+  newNode.next = prevFirst;
+  return;
+};
 
 LRUCache.prototype.get = function (key) {
-  if (this.myHash[key]){
-
-    // if its already at front then don't move it
-    if (this.myHash[key] === this.head.next) {
-      return this.myHash[key].value;
-    }
-
-    // move to front function
-
-    // movingNode = node moving to front
-    // prevFirst = previous first in line
-    let prevFirst = this.head.next;
-    let movingNode = this.myHash[key];
-
-    // cut out the moving node first
-    let movePrev = movingNode.prev;
-    let moveNext = movingNode.next;
-    movePrev.next = moveNext;
-    moveNext.prev = movePrev;
-
-    // insert moving at front
-    this.head.next = movingNode;
-    movingNode.prev = this.head;
-    movingNode.next = prevFirst;
-    prevFirst.prev = movingNode;
-
-    // return the value of the key
-    return movingNode.value;
-
-    
+  // get operation accesses the hash and returns hash[key]
+  // also need to move the node to be next to head 
+  // use helper function moveFront
+  // if its already at front then return the val
+  // not at front, move to front function and return val
+  if (this.myHash[key]){   
+    this.moveFront(key);
+    return this.myHash[key].val;    
   } else {
     return -1;
-  }
-
+  };
 };
 
-
 LRUCache.prototype.put = function (key, value) {
-  // if key exists, move to front with new value
+  // if key exists, move to front, rewrite value
   // else, insert new key value pair at front
   // also, check if length of the LRU cache is at limit
 
-  if(this.myHash[key]){ 
-
-    // move to front function
-
-    // movingNode = node moving to front
-    // prevFirst = previous first in line
-    let prevFirst = this.head.next;
-    let movingNode = this.myHash[key];
-
-    // cut out the moving node first
-    let movePrev = movingNode.prev;
-    let moveNext = movingNode.next;
-    movePrev.next = moveNext;
-    moveNext.prev = movePrev;
-
-    // insert moving at front
-    this.head.next = movingNode;
-    movingNode.prev = this.head;
-    movingNode.next = prevFirst;
-    prevFirst.prev = movingNode;
-
-    // now the nodes have been replaced
-
+  if(this.myHash[key]){
+    // move to front function then rewrite the value
+    this.moveFront(key);
+    this.myHash[key].val = value;
   } else {
-    if (this.length >= this.capacity) {
-      // new key and capacity reached
-      // evict last node
-      // place new node at front
-
-      // evict last node and delete the key from hash
-      let evictNode = this.tail.prev;
-      this.tail.prev = evictNode.prev;
-      evictNode.prev.next = this.tail;
-      delete this.myHash[evictNode.key];
-
-      // create new node and place new node at front
-      this.myHash[key] = new Node(key, value);
-      let movingNode = this.myHash[key];
-      let prevFirst = this.head.next;
-      this.head.next = movingNode;
-      movingNode.prev = this.head;
-      movingNode.next = prevFirst;
-      prevFirst.prev = movingNode;
-
-    } else {
-      // function insertAtFront()
-      this.myHash[key] = new Node(key, value);
-      let movingNode = this.myHash[key];
-
-
-      let prevFirst = this.head.next;
-
-      this.head.next = movingNode;
-      movingNode.prev = this.head;
-
-      movingNode.next = prevFirst;
-      prevFirst.prev = movingNode;
-
-      this.length++;
-      
-    } 
-  }
-
+    // insert new node at front
+    let newNode = new Node(key, value);
+    this.myHash[key] = newNode;
+    this.insertFront(newNode);
+    this.length++;
+    // check if max length reached and delete accordingly
+    if(this.length > this.capacity){
+      this.deleteLast();
+      this.length--;
+    }
+  };
+  return;
 };
 
-
-class Node {
-  constructor(key, value){
-    this.key = key;
-    this.value = value;
-    this.prev = null;
-    this.next = null;
-  }
-}
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
 
 let cache = new LRUCache(2);
 cache.put(2,1);
 cache.put(2, 2);
-cache.get(2);
+console.log(cache.get(2));
 cache.put(1, 1);
 cache.put(4, 1);
 cache.get(2);
--- interview_q/48.js
+++ interview_q/48.js
@@ -7,9 +7,7 @@ var rotate = function (matrix) {
       for(let j = i + 1; j < n; j++){
 		  [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
 	  };
-	};
-	for(let i = 0; i < n; i++){
-		matrix[i].reverse();
+	  matrix[i].reverse();
 	};
 	return matrix;
 };
