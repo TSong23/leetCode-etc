d8558012dd5b68f95636e5e2d735df8e3a6fb40f
Taehoon <songyi3279@gmail.com>
2020-08-02 18:34:41 -0700

    word break debuggins took so long

--- interview_q/leetcode_79.js
+++ interview_q/leetcode_79.js
@@ -9,62 +9,53 @@
 // Given word = "SEE", return true.
 // Given word = "ABCB", return false.
 
+// cannot reuse cells!
+
 // we have to take the first letter, find all instances of it in matrix
-// start BFS from that letter.
+// start DFS from that letter.
+// keep track of indexes visited via a set
+// keep track of letters to find in an array
+// DFS should return false if i,j is out of bounds
+// return false if board[i][j] !== DFSletters[0]
+
 
 var exist = function (board, word) {
-  const rowL = board.length;
-  const colL = board[0].length;
-  let myWord = word.split('');
-  let myAns = false; 
-  let myHash = {};
-  
+	if (board.length === 0) return false;
 
-  function myBFS(bfsWord, i, j){
-    if(myHash[`${i},${j}`]) return false;
-    myHash[`${i},${j}`] = true;
-    if(!bfsWord.length) return true;
-    let up,down,left,right;
+	function myDFS(i, j, k) {
+		if (board[i][j] !== word[k]) return false;
+		if (k === word.length - 1) return true;
+		console.log(i,j,k);
+		board[i][j] = '*';		
+		
+		//begin DFS in U,D,L,R
+		for(let [dx,dy] of dirs){
+			let x = i + dx;
+			let y = j + dy;
+			if(x >= 0 && x < rowL && y >= 0 && y < colL){
+				if(myDFS(x,y,k+1)) return true;
+			};
+		};
+		board[i][j] = word[k]; // reset the board
+		return false;
+	};
 
-    if (board[i+1] && board[i+1][j] === bfsWord[0]){
-      bfsWord.shift();
-      right = myBFS(bfsWord,i+1,j);
-    } else if (board[i-1] && board[i-1][j] === bfsWord[0]){
-      bfsWord.shift();
-      left = myBFS(bfsWord, i-1, j);
-    } else if (board[i][j+1] && board[i][j+1] === bfsWord[0]){
-      bfsWord.shift();
-      up = myBFS(bfsWord, i, j+1);
-    } else if (board[i][j-1] && board[i][j-1] === bfsWord[0]){
-      bfsWord.shift();
-      down = myBFS(bfsWord,i,j-1);
-    };
-
-    if(up || down || left || right) return true;
-    return false; 
-
-  };
-
-  for(let i = 0; i < rowL; i++){
-    if(myAns) return true;
-    for (let j = 0; j < colL; j++) {
-      if(board[i][j] === word[0]){
-        myAns = myBFS(myWord, i, j);
-        if(myAns){
-          return true;
-        }else{
-          myHash = {};
-          myWord = word.split('');
-        };
-      };
-    };
-  };
-
-  return myAns; 
+	const rowL = board.length;
+	const colL = board[0].length;
+	const dirs = [[-1,0],[1,0],[0,-1],[0,1]];
+	for(let i = 0; i < rowL; i++){
+		for(let j = 0; j < colL; j++){
+			if( myDFS(i,j,0)) return true;
+		};
+	};
+	return false;
 };
 
-console.log(exist([
-  ["C", "A", "A"], 
-  ["A", "A", "A"], 
-  ["B", "C", "D"]],
-"AAB"));
+
+console.log(exist(
+	[["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]],
+"ABCCED"));
+
+
+
+
