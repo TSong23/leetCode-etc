6da86c4d167d70617374f9b30fa78e009d59c088
Taehoon <songyi3279@gmail.com>
2020-08-03 18:28:23 -0700

    coin change solved

--- dynamic_programming/coin_change.js
+++ dynamic_programming/coin_change.js
@@ -1,14 +1,26 @@
+// You are given coins of different denominations and a total amount of money amount.Write a function to compute the fewest number of coins that you need to make up that amount.If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// lets try bottom up tabulation approach
+// array goes up to N; if not accessible, -1
+// each entry checks if it can get there from a previous entry + one of the coins
+
+
+
 let coinChange = (coins, amount) => {
-    let N = A.length;
-    let dp = [...Array(N + 1)].map(_ => Array(T + 1).fill(Infinity));
-    for (let i = 0; i <= N; ++i)
-        dp[i][0] = 0;
-    for (let i = 1; i <= N; ++i)
-        for (let t = 1; t <= T; ++t)
-            dp[i][t] = Math.min(1 + (0 <= t - A[i - 1] ? dp[i][t - A[i - 1]] : Infinity), dp[i - 1][t]); 
-            // min(1 + with or without)
-    return dp[N][T] < Infinity ? dp[N][T] : -1;
+    const dp = Array(amount + 1).fill(Infinity);
+    dp[0] = 0;
+    for (let i = 1; i <= amount; i++) {
+        for (const coin of coins) {
+            if (i - coin >= 0) {
+                dp[i] = Math.min(
+                    dp[i],
+                    dp[i - coin] + 1,
+                );
+            }
+        }
+    }
+    return dp[amount] === Infinity ? -1 : dp[amount];    
 };
 
-console.log([1,2,5],11);
+console.log(coinChange([1, 2147483647],2));
 
