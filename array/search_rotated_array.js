5c938e5e0d2c65c290ff2a6e61dd42fa6e14d235
Taehoon <songyi3279@gmail.com>
2020-08-06 22:09:13 -0700

    solved search rotated array

--- array/search_rotated_array.js
+++ array/search_rotated_array.js
@@ -1,42 +1,27 @@
 // Input: nums = [4, 5, 6, 7, 0, 1, 2], target = 0
 // Output: 4
 
-// search from end if the end is less than the start
-// calculate the rotated amount
-// find the target
-var search = function (nums, target) {
+// need to modify binary search
+// divide in to two
+// check the first and last element of divided sections
+// first <= target <= last ; first <= target <= last
 
+var search = function (nums, target) {
   if (!nums.length) return -1;
-  if (nums.length < 2) {
-    if (nums[0] === target) {
-      return 0;
-    } else {
-      return -1;
-    };
-  };
-  let numRo;
-  let numLen = nums.length;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[numLen - i] > nums[0]) {
-      numRo = i;
-      break;
-    };
-  };
-  let spliceArr = nums.splice(numRo);
-  let newArr = spliceArr.concat(nums);
-  let myIdx = newArr.indexOf(target);
-  if (myIdx < 0) return -1;
-  console.log('numRo:', numRo);
-  console.log('numLen:', numLen);
-  if (numRo >= numLen - 1) {
-    return myIdx;
-  } else {
-    return myIdx + numRo;
-  }
+  let len = nums.length;
+  if( target < nums[0] && target > nums[len] ) return -1;
+
+  let mdpt = Math.floor(len/2);
+  let mid = nums[mdpt];
+  if(mid === target) return mdpt;
+
+  let left =  search(nums.slice(0,mdpt),target);
+  let right = search(nums.slice(mdpt+1),target);
+
+  if( left !== -1) return left;
+  if( right !== -1) return mdpt + right + 1;
+  return -1;  
 };
 
-console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
+console.log(search([1], 1));
 
-// still in progress?
-// nah not really anymore
-// square fill
