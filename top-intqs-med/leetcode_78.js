863a31c18fdf7eb23804e9d473908ed53a062b84
Taehoon <songyi3279@gmail.com>
2020-08-13 23:48:33 -0700

    leetcode 78 solved

--- top-intqs-med/leetcode_78.js
+++ top-intqs-med/leetcode_78.js
@@ -18,38 +18,25 @@
 // ]
 
 // we can achieve the iteratively
-// outer loop consists of how many we are grouping each time
-// inner loop tracks where the starting position is
-// 2nd inner loop tracks corresponding start position (what we really want is subset(j, [k -> k+i]))
-// start: k = j, end k = j + i
+// Using[1, 2, 3] as an example, the iterative process is like:
 
-/*
-    1 2 3 4 5
+// Initially, one empty subset[[]]
+// Adding 1 to[]: [[], [1]];
+// Adding 2 to[] and[1]: [[], [1], [2], [1, 2]];
+// Adding 3 to[], [1], [2] and[1, 2]: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].
 
-    i = 2
-        j = 0, k = 0, [1, 2]  0 <= k < 2 nums[0] and nums[1]
-        j = 0, k = 2, [1, 3]
-        j = 0, k = 3, [1, 4]
-        j = 0, k = 4, [1, 5]
-        j = 1, k = 2, [2, 3]
-        j = 1, k = 3, [2, 4]
-        j = 1, k = 4, [2, 5]
-        j = 2, k = 3, [3, 4]
-        j = 2, k = 4, [3, 5]
-        j = 3, k = 4, [4, 5]
 
-    i = 3
-        j = 0, k = 2, [1, 2, 3]
-        j = 0, k = 2, [1, 3]
-        j = 0, k = 3, [1, 4]
-*/
 var subsets = function (nums) {
-    let ansArr = [];
-    let holdArr = [];
-    for (let i = 1; i < nums.length; i++){
-        holdArr = [];
-        for(let j = 0; j < nums.length - i; j++){
-            for(let k = j+1; )
+    let ansArr = [[]];
+    let holdLen, holdVar;
+    for (let i = 0; i < nums.length; i++){
+        holdLen = ansArr.length;
+        for(let j = 0; j < holdLen; j++){
+            holdVar = ansArr[j].concat([nums[i]]);
+            ansArr.push(holdVar);
         };
     };
+    return ansArr;
 };
+
+subsets([1,2,3])
