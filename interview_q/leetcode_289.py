7e94daa2d02fe810b65b974df524b9a497b91b65
Taehoon <songyi3279@gmail.com>
2020-08-16 15:21:30 -0700

    solved leetcode 289 in python. also figured out how to configure and debug python in vscode

--- interview_q/leetcode_289.py
+++ interview_q/leetcode_289.py
@@ -12,42 +12,42 @@ For Loop: determine state of each cell using helper function
 
 class Solution:
     def gameOfLife(self, board):
+        """
+        Do not return anything, modify board in-place instead.
+        """
         row, col = len(board), len(board[0])
-        def helper(rIdx, cIdx):
-            start = board[rIdx][cIdx]
-            count = 0
-            if(board[rIdx - 1][cIdx - 1] and board[rIdx - 1][cIdx - 1] == 1):
-                count += 1
-            if(board[rIdx - 1][cIdx + 1] and board[rIdx - 1][cIdx - 1] == 1):
-                count += 1
-            if(board[rIdx + 1][cIdx - 1] and board[rIdx - 1][cIdx - 1] == 1):
-                count += 1
-            if(board[rIdx + 1][cIdx + 1] and board[rIdx - 1][cIdx - 1] == 1):
-                count += 1
-            if(board[rIdx - 1][cIdx] and board[rIdx - 1][cIdx - 1] == 1):
-                count += 1
-            if(board[rIdx + 1][cIdx] and board[rIdx - 1][cIdx - 1] == 1):
-                count += 1
-            if(board[rIdx][cIdx - 1] and board[rIdx - 1][cIdx - 1] == 1):
-                count += 1
-            if(board[rIdx][cIdx + 1] and board[rIdx - 1][cIdx - 1] == 1):
-                count += 1   
-            if(start == 0):
-                if(count == 3): return 1
-            else:
-                if(count < 2 or count > 3): return 0
-                else: return 1
-    
-
-        helperArr = [ [0]*len(board) for i in range(row)]
+        helperArr = [[0]*col for i in range(row)]
         for i in range(row):
             for j in range(col):
-                helperArr[i][j] = helper(i,j)
+                helperArr[i][j] = self.helper(board,i, j,row,col)
+ 
 
         for i in range(row):
             for j in range(col):
                 board[i][j] = helperArr[i][j]
 
-        return
+    def helper(self, board, i, j,row,col):
+        start = board[i][j]
+        count = 0
+        if i - 1 >= 0 and j - 1 >= 0 and board[i-1][j-1] == 1: count += 1
+        if i - 1 >= 0 and j + 1 < col and board[i-1][j+1] == 1: count += 1
+        if i + 1 < row and j - 1 >= 0 and board[i+1][j-1] == 1: count += 1
+        if i + 1 < row and j + 1 < col and board[i+1][j+1] == 1: count += 1
+        if i - 1 >= 0  and board[i-1][j] == 1: count += 1
+        if i + 1 < row  and board[i+1][j] == 1: count += 1
+        if j - 1 >= 0  and board[i][j-1] == 1: count += 1
+        if j + 1 < col  and board[i][j+1] == 1: count += 1
+        if(start == 0):
+            if(count == 3): return 1
+            return 0
+        else:
+            if(count < 2 or count > 3): return 0
+            return 1
 
-    
+test = Solution()
+test.gameOfLife([
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0]
+])
