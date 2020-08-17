b6acc35b1016d3115041d0124e9faff60ee51c94
Taehoon <songyi3279@gmail.com>
2020-08-17 00:28:17 -0700

    solved leetcode 378 in trivial way

--- interview_q/leetcode_378.py
+++ interview_q/leetcode_378.py
@@ -1,12 +1,37 @@
+"""
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+return 13.
+
+
+[
+[1,2],
+[1,3]
+]
+k = 2,
+return 1
+
+"""
+# the last element of a row is not necessarily smaller than the first element of next row
+# why not push every element in to an array, sort it, then return k from end?
+# works and its 82.16% faster
+
 class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        n, m = len(matrix), len(matrix[0])
-        if k < m:
-            return matrix[0][k-1]
-        count, i = 0, 0
-        while count < k:
-            count += m
-            i += 1
-        diff = m*i - k
-        print(m, i, diff)
-        return matrix[i-1][m - diff - 1]
+    def kthSmallest(self, matrix, k):
+        row = len(matrix)
+        col = len(matrix[0])
+        reArr = []
+        for i in range(row):
+            for j in range(col):
+                reArr.append(matrix[i][j])
+        reArr.sort()
+        return reArr[k-1]
+
+
+# the binary or heap solutions:
+# https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/301357/Java-0ms-(added-Python-and-C%2B%2B)%3A-Easy-to-understand-solutions-using-Heap-and-Binary-Search
