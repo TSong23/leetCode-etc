4fa06bbcd89bcf6ebb9f5beb9379dfe7b1420309
Taehoon <songyi3279@gmail.com>
2020-08-30 22:23:21 -0700

    162 solved

--- interview_q/leetcode_162.js
+++ interview_q/leetcode_162.js
@@ -12,15 +12,11 @@ You may imagine that nums[-1] = nums[n] = -∞.
 // left and right returns the index
 
 var findPeakElement = function (nums) {
-    if (!nums.length) return null;
     nums.push(-Infinity);
     nums.unshift(-Infinity);
-    let len = nums.length;
-    let mid = Math.floor(len/2);
-    if(nums[mid-1] < nums[mid] && nums[mid+1] < nums[mid]) return mid-1;
-    let left = findPeakElement(nums.slice(1,mid));
-    let right = findPeakElement(nums.slice(mid+1,len-1));
-    if(left) return left;
-    if(right) return mid + right - 1;
-    return null;  
+    for (let i = 1; i < nums.length-1; i++){
+        if((nums[i] > nums[i-1])&&(nums[i]>nums[i+1])) return i-1;
+    };
 };
+
+findPeakElement([1, 2, 3])
