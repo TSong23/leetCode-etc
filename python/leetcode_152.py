f1b953ee9de7b8b464a7afdc5a6a470566d6034b
Taehoon <songyi3279@gmail.com>
2020-09-03 19:52:50 -0700

    leetcode 152

--- python/leetcode_152.py
+++ python/leetcode_152.py
@@ -2,9 +2,28 @@
 Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
 
 use kadane's algo
+if multiplying cur number to cursum increases, do it. then compare to max
+if not, start a new subArr
+
+variables: 
+    globalMax
+    maxValue: use abs value to determine size. at the end, if this is
+            positive, return this
+    localMax: if localMax exceeds globalMax, replace
+
+Constant Memory
+N time
+
 """
 
 class Solution:
     def maxProduct(self, nums):
-        
+        localMax, maxValue, globalMax = nums[0], nums[0], nums[0]
+        for i in range(1, len(nums)):      
+            
+
         return
+
+
+test = Solution()
+test.maxProduct([2, 3, -2, 4])
