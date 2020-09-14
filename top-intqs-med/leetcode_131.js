56b7cf21f6bdb93ba4ab0c520d8f43d26591753f
Taehoon <songyi3279@gmail.com>
2020-09-13 23:20:26 -0700

    leetcode partitioning problems

--- top-intqs-med/leetcode_131.js
+++ top-intqs-med/leetcode_131.js
@@ -13,7 +13,19 @@ Output:
 ]
 
 data struc: 2d arr
-suggests that i use recursion and each end point returns an array
+backt racking question
+
+if the input is "aab", check if [0,0] "a" is palindrome. then check [0,1] "aa", then [0,2] "aab".
+While checking [0,0], the rest of string is "ab", use ab as input to make a recursive call.
+if the input is "aab", check if [0,0] "a" is palindrome. then check [0,1] "aa", then [0,2] "aab". While checking [0,0], the rest of string is "ab", use ab as input to make a recursive call. ![enter image description here][1]
+
+in this example, in the loop of i=l+1, a recursive call will be made with input = "ab".
+Every time a recursive call is made, the position of l move right.
+
+How to define a correct answer?
+Think about DFS, if the current string to be checked (Palindrome) contains the last position, in this case "c", this path is a correct answer, otherwise, it's a false answer.
+
+
 
 this is a really hard problem
 
@@ -24,4 +36,8 @@ this is a really hard problem
 
 var partition = function (s) {
 
+
+
 };
+
+console.log(partition())
