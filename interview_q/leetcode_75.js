696052cc99b70f8a09567bb0bfd9cfe59f1478b5
Taehoon <songyi3279@gmail.com>
2020-09-18 21:13:02 -0700

    leetcode 75 begin

---
+++ interview_q/leetcode_75.js
@@ -0,0 +1,47 @@
+/*
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+[2,0,2,1,1,0]   o and R swap, guranteeing that R is now 2
+ L   o t   R    R move left one.
+[2,0,0,1,1,2]
+ L   o t R      o and L swap, guranteeing that left most is 0, L moves right
+[0,0,2,1,1,2]
+   L o t R      o and R swap. 
+     
+1. two pointers start at middle: zero pointer and two pointer
+2. pointers move to next when current idx becomes 1
+3. if its not, swap the number with the left and right ends
+
+data struc: left, right, zero, 2
+zero and two does the same: when it hits zero, send to left idx
+if it hits a two, send to right idx
+*/
+
+
+var sortColors = function (nums) {
+    let L = 0;
+    let R = nums.length - 1;
+    let two = Math.round(nums.length/2);
+    let zero = two - 1;
+
+    function helper(idx){
+        if(nums[idx] === 0){
+            [ nums[idx], nums[L]] = [ nums[L], nums[idx]];
+            L++;
+        } else{
+            [nums[idx], nums[R]] = [nums[R], nums[idx]];
+            R--;
+        };
+        return;
+    };
+
+    while(L < zero || R > two){
+        nums[zero] === 1 ? zero-- : helper(zero);
+        nums[two] === 1 ? two++ : helper(two);
+    };
+
+    return nums;
+};
+
+sortColors([2,0,2,1,1,0])
