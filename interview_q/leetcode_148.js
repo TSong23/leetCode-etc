f0c9643e3769dfdf7e903836b1a143c255086d69
Taehoon <songyi3279@gmail.com>
2020-09-22 02:00:36 -0700

    leetocde 148

---
+++ interview_q/leetcode_148.js
@@ -0,0 +1,33 @@
+/*
+Given the head of a linked list, return the list after sorting it in ascending order.
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+initial thoughts: 
+
+keep track of start node, current node
+
+the first time, current node is < start node, start node is fixed
+send all nodes that are less than start node to be in front of start node
+reloop and determine a new start node
+
+when the end is reached without having defined a start node, the list is sorted
+use a helper function
+
+outer loop:
+*/
+
+
+var sortList = function (head) {
+    let start;
+    let cur;
+    while(cur){
+        if(cur < start){
+            // insert cur to be in front of start
+            // reset
+        }else{
+            cur = cur.next
+        }
+    }
+
+
+};
