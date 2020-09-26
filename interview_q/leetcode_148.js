953a7579ed3d2003b6871f978fbefc936bc731be
Taehoon <songyi3279@gmail.com>
2020-09-25 23:31:00 -0700

    update

--- interview_q/leetcode_148.js
+++ interview_q/leetcode_148.js
@@ -14,16 +14,16 @@ while two pointers that start at head1 and head2 arent null, compare the two poi
 and add to the list
 return the head of the list
 
-
+still not getting it
 */
 
 
 var sortList = function (head) {
-    if(!head.next) return head;
+    if(!head || !head.next) return head;
 
     let fast = head;
     let slow = head;
-    while(fast){
+    while(fast && fast.next){
         slow = slow.next;
         fast = fast.next.next;
     };
@@ -33,8 +33,18 @@ var sortList = function (head) {
     let left = sortList(head);
     let right = sortList(head2);
 
-
+    let newHead = new ListNode();
     while(left || right){
-
-    }
+        if (left.val > right.val) {
+            newHead.next = left;
+            left = left.next;
+        } else {
+            newHead = right;
+            right.next;
+        };
+        newHead = newHead.next;
+    };
+    if(left) newHead.next = left;
+    if(right) newHead.next = right;
+    return newHead.next;
 };
